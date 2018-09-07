c7386e01af099e5a428607854662e3617de52899
RashmiRB <rbidanta@yahoo.in>
2018-09-07 11:52:51 -0400

    Count Down App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Count Down App';
+  countDownTo: Date;
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+  countdown;
+
+  ngOnInit(){
+    this.days = 0;
+    this.hours = 0;
+    this.minutes = 0;
+    this.seconds = 0;
+  }
+
+  /**
+   * This function is called when a date is selected in the datepicker
+   */
+  onSelectCountDownToDate(){
+      var self = this // Capture the current scope in self variable to be used in anonymous function
+      this.countdown = setInterval(function(){
+      var timenow:number = new Date().getTime();
+      var diff:number = self.countDownTo.getTime() - timenow;
+      self.days = Math.floor(diff/(1000*60*60*24));
+      self.hours = Math.floor((diff%(1000*60*60*24))/(1000*60*60));
+      self.minutes = Math.floor((diff%(1000*60*60))/(1000*60));
+      self.seconds = Math.floor((diff%(1000*60))/(1000));
+      if(diff < 0){
+        clearInterval(self.countdown);
+        self.days = 0;
+        self.hours = 0;
+        self.minutes = 0;
+        self.seconds = 0;
+      }
+    },1000);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {CalendarModule} from 'primeng/calendar';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    CommonModule,
+    FormsModule,
+    BrowserModule,
+    CalendarModule,
+    NgbModule,
   ],
-  providers: [],
+  providers: [],  
   bootstrap: [AppComponent]
 })
 export class AppModule { }
